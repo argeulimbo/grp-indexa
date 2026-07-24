4d594b99cc0f79c8da03d21544a347a4877acbdf
Argeu Souza <argeusousaptbr@gmail.com>
2026-07-24 10:50:46 -0300

    feat: end of first course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,49 @@ import { ContainerComponent } from "./componentes/container/container.component"
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { SeparadorComponent } from "./componentes/separador/separador.component";
 import { ContatoComponent } from "./componentes/contato/contato.component";
+import { FormsModule } from '@angular/forms';
+
+import agenda from './agenda.json';
+
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-            CommonModule,
-            RouterOutlet,
-            ContainerComponent,
-            CabecalhoComponent,
-            SeparadorComponent,
-            ContatoComponent
-           ],
+    CommonModule,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda;
+
+  filtroPorTexto: string = '';
+
+  filtrarContatosPorTexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos
+    }
+    return this.contatos.filter(contato => {
+      return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase());
+    })
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter( contato => {
+      return contato.nome.toLowerCase().startsWith(letra)
+    } )
+  }
 }
