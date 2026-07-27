58ce6925604ab3462f119e13d569995240af1312
Argeu Souza <argeusousaptbr@gmail.com>
2026-07-27 15:58:50 -0300

    feat: add validators

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from "../../componentes/container/container.component";
+import { SeparadorComponent } from "../../componentes/separador/separador.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
   standalone: true,
-  imports: [ContainerComponent],
+  imports: [
+            ContainerComponent,
+            SeparadorComponent,
+            ReactiveFormsModule
+          ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
 
+  contatoForm!: FormGroup;
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome:         new FormControl('', Validators.required),
+      telefone:     new FormControl('', Validators.required),
+      email:        new FormControl('', [Validators.required, Validators.email]),
+      aniversario:  new FormControl(''),
+      redes:        new FormControl(),
+      observacoes:  new FormControl()
+    })
+  }
+
+  salvarContato() {
+    if (this.contatoForm.valid) {
+      console.log(this.contatoForm.value);
+    }
+    else {
+      alert('Preencha os dados corretamente!')
+    }
+  }
+
+  cancelar() {
+    this.contatoForm.reset();
+    console.log('Form reset active')
+  }
 }
